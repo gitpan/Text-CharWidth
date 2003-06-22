d8c2323b5fb99b109ee7c420d9a0d0380081b80d
久保田 智広 <kubota@debian.org>
2003-06-22 00:46:42 +0000

    Import of KUBOTA/Text-CharWidth-0.03 from CPAN.
    gitpan-cpan-distribution: Text-CharWidth
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         KUBOTA/Text-CharWidth-0.03.tar.gz
    gitpan-cpan-author:       KUBOTA
    gitpan-cpan-maturity:     released

--- CharWidth.pm
+++ CharWidth.pm
@@ -11,7 +11,7 @@ our @EXPORT_OK = qw(mbwidth mbswidth mblen);
 our @EXPORT = qw();
 our %EXPORT_TAGS = ('all' => [ @EXPORT_OK ]);
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 require XSLoader;
 XSLoader::load('Text::CharWidth', $VERSION);
